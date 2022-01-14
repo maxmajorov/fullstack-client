8768d6e58fb9acfdf4df131f7a60140258f0ebb2
Maxim Predko <maxpredko@gmail.com>
2022-01-14 21:49:50 +0300

    init: start my-react-app with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      It's my first react app with typescript
+      <Rating />
     </div>
   );
 }
+
+function Rating() {
+
+  return (
+    <div>
+      <RatingTitle/>
+      <RatingBody/>
+    </div>
+  )
+}
+
+function Star() {
+  return (
+    <div>
+      <span>Star</span>
+    </div>
+  )
+}
+
+function RatingTitle() {
+  return (
+    <h3>MENU</h3>
+  )
+}
+
+function RatingBody() {
+  return (
+      <ul>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+      </ul>
+  )
+}
+
 
 export default App;
